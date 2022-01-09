98ffd9514e478976fa4c9de2129db311a3c521b9
John Cowan <johnc.livinglife@gmail.com>
2022-01-09 09:45:30 -0500

    Add Supplier functionality

--- MPA.Web/Pages/supplier.cshtml.cs
+++ MPA.Web/Pages/supplier.cshtml.cs
@@ -23,4 +23,19 @@ public class supplierModel : PageModel
         Suppliers = db.Suppliers.OrderBy(c => c.Country).ThenBy(c => c.CompanyName);
 
     }
+
+    [BindProperty]
+    public Supplier? Supplier { get; set; }
+
+    public IActionResult OnPost()
+    {
+        if ((Supplier is not null) && ModelState.IsValid)
+        {
+            db.Suppliers.Add(Supplier);
+            db.SaveChanges();
+            return RedirectToPage("/suppliers");
+        }
+        
+        return Page();
+    }
 }
