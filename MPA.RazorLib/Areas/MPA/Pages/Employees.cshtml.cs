9ea6565c8170029cb62ec7e259a9c98080a1125e
John Cowan <johnc.livinglife@gmail.com>
2022-01-09 14:02:25 -0500

    Added Employee and Orders page

---
+++ MPA.RazorLib/Areas/MPA/Pages/Employees.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MPA.Data;
+
+namespace MPA.RazorLib.MPA.Pages;
+
+public class EmployeesPageModel : PageModel
+{
+    private MPAContext db;
+
+    public EmployeesPageModel(MPAContext injectedContext)
+    {
+        db = injectedContext;
+    }
+
+    public IQueryable<Employee>? Employees { get; set; } = null!;
+
+    public void OnGet()
+    {
+        ViewData["Title"] = "MPA - Employees";
+        Employees = db.Employees.OrderBy(c => c.Country).ThenBy(c => c.LastName).ThenBy(c => c.FirstName);
+
+    }
+}
